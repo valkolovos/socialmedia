cc9deadd9ee6bb57cd1ff71a8f0ad909dff3f4f5
valkolovos <val.kolovos@guildeducation.com>
2022-07-16 09:41:09 -0600

    updating repo name for auto-update docker image

--- update/main.py
+++ update/main.py
@@ -64,6 +64,21 @@ def main():
         retry_command('gcloud secrets versions add backend-sha --data-file="backend-sha.json"')
         print('Done writing current SHA to GCP secrets')
 
+        existing_jobs = retry_command('gcloud beta run jobs list')
+        job_region = 'us-central1'
+        backend_image = 'us-central1-docker.pkg.dev/freme-2022/freme-backend-update/freme-backend-update:latest'
+        if 'freme-backend-update' not in existing_jobs:
+            backend_cmd = 'create'
+        else:
+            backend_cmd = 'update'
+        print(f'{"Creating" if backend_cmd=="create" else "Updating"} backend update job...')
+        retry_command(
+            f'gcloud beta run jobs {backend_cmd} freme-backend-update '
+            f'--image={backend_image} '
+            f'--region={job_region} --service-account={project_name}@appspot.gserviceaccount.com --quiet'
+        )
+        print(f'Done {"creating" if backend_cmd=="create" else "updating"} backend update job')
+
     except Exception as e:
         raise e
     finally:
